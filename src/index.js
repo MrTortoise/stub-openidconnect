9bf5b5aa236091901bd8424407a1a53df9764fbf
John Nicholas <themrtortoise@gmail.com>
2024-01-30 21:24:44 +0000

    v basic mock to access token

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+
 import express from 'express';
+import {  format } from 'url';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/authorize', (req, res) => {
+    const { query } = req
+    console.log(query)
+    res.redirect(format({
+        pathname: query.redirect_uri,
+        query: {
+            code: 'authcode', state: query.state
+        }
+    }))
+})
+
+app.get('/callback', async (req, res) => {
+    console.log('in callback')
+    res.send('callback')
+})
+
+app.post('/token', (req, res) => {        
+    res.json({access_token:'token', token_type: 'bearer'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,18 @@
+
+const url = require('url')
+
 describe('running service on 3000 will', () => {
-    it('return authorization code on hitting authorize endpoint', () => {
-expect(true).toBe(false)
+    it('return authorization code on hitting authorize endpoint and goes to callback', async () => {
+        await fetch('http://localhost:3000/authorize?client_id=kbyuFDidLLm280LIwVFiazOqjO3ty8KH&redirect_uri=http://localhost:3000/callback&scope=openid profile email phone address&response_type=code&state=dff76686113544fb79cc6c48a0b0c2a7494b5484')
+            .then(response => response.text())
+            .then(data => expect(data).toBe('callback'))
+            .catch(error => expect(error).toBe('error'));
+    })
+
+    it('exchanges the access token', async () => {
+        await fetch('http://localhost:3000/token?response_type=token&client_id=dave', { method: "POST" })
+            .then(response => response.json())
+            .then(data => expect(data).toEqual({ access_token: 'token', token_type: 'bearer' }))
+            .catch(error => expect(error).toBe('error'));
     })
 })
